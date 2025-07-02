4e15cc225a225d3da2993f3825b618167f9d765b
Amadou Bagayoko <abagayoko304@gmail.com>
2025-07-02 10:42:58 +0000

    creation des fichiers

---
+++ ApiiCollabdev/src/main/java/com/example/ApiiCollabdev/Controller/IdeeProjetController.java
@@ -0,0 +1,4 @@
+package com.example.ApiiCollabdev.Controller;
+
+public class IdeeProjetController {
+}
--- ApiiCollabdev/src/main/java/com/example/ApiiCollabdev/Repository/IdeeProjetRepository.java
+++ ApiiCollabdev/src/main/java/com/example/ApiiCollabdev/Repository/IdeeProjetRepository.java
@@ -1,4 +1,8 @@
 package com.example.ApiiCollabdev.Repository;
 
-public interface IdeeProjetRepository {
+import com.example.ApiiCollabdev.entities.IdeeProjet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IdeeProjetRepository extends JpaRepository<IdeeProjet,Integer> {
+
 }
