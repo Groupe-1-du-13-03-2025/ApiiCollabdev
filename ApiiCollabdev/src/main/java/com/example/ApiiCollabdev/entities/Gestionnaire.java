fed6b5c9303259c2b1452229c176ada43cb99ed5
Nantenin Keita <knanete@gmail.com>
2025-07-02 09:41:36 +0000

    commit de création du projet et sa relation avec le Gestionnaire

--- ApiiCollabdev/src/main/java/com/example/ApiiCollabdev/entities/Gestionnaire.java
+++ ApiiCollabdev/src/main/java/com/example/ApiiCollabdev/entities/Gestionnaire.java
@@ -3,6 +3,8 @@ package com.example.ApiiCollabdev.entities;
 import com.example.ApiiCollabdev.entities.enums.Genre;
 import com.example.ApiiCollabdev.entities.enums.Role;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MappedSuperclass;
 import lombok.*;
 
@@ -16,6 +18,7 @@ public class Gestionnaire extends Utilisateur {
     public Gestionnaire(int id, String prenom, String nom, String email, String motDePasse, Genre genre, Role role, String uriCv) {
         super(id, prenom, nom, email, motDePasse, genre, role);
         this.uriCv = uriCv;
+
     }
 }
 
