73541d7703ca8651502e6d794e214ef63a9731ed
Djimmoh <dembeledjime83@gmail.com>
2025-07-02 14:09:44 +0000

    Nouvelle ModificationÃ

--- ApiiCollabdev/src/main/java/com/example/ApiiCollabdev/Repository/PorteurProjetRepository.java
+++ ApiiCollabdev/src/main/java/com/example/ApiiCollabdev/Repository/PorteurProjetRepository.java
@@ -1,6 +1,7 @@
 package com.example.ApiiCollabdev.Repository;
 
 import com.example.ApiiCollabdev.entities.PorteurProjet;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PorteurProjetRepository extends CrudRepository<PorteurProjet, Long> {
--- ApiiCollabdev/src/main/java/com/example/ApiiCollabdev/entities/PorteurProjet.java
+++ ApiiCollabdev/src/main/java/com/example/ApiiCollabdev/entities/PorteurProjet.java
@@ -2,8 +2,16 @@ package com.example.ApiiCollabdev.entities;
 
 import com.example.ApiiCollabdev.entities.enums.Genre;
 import com.example.ApiiCollabdev.entities.enums.Role;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "porteur_projet")
 public class PorteurProjet extends Utilisateur{
+
+
+    public PorteurProjet() {
+    }
     public PorteurProjet(long id, String prenom, String nom, String email, String motDePasse, Genre genre, Role role) {
         super(id, prenom, nom, email, motDePasse, genre, role);
     }
